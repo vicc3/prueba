23b9d9ff25c8fc4c090fb920efad78bb811a68f9
vico <victoria.valenzuelaaa@gmail.com>
2024-03-14 19:34:08 -0700

    Se agregó botón, label e input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form.Label>happy halloween</Form.Label>
+        <Form.Control></Form.Control>
+        <Button variant="primary">press the button if you said hi</Button>{''}
       </header>
     </div>
   );
