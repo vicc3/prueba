60207854a9449e602a0d4f4bd23e772e1cca280c
vico <victoria.valenzuelaaa@gmail.com>
2024-03-15 08:59:09 -0700

    Primer formulario, acción al botón

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
 import logo from './rayo.jpeg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import Primer_forms from './componentes/Primer_form';
+import { useState } from 'react';
 
 function App() {
+  const [ carroNombre, setCarro ] = useState("Nombre");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className = "App-link"
-          href = "https://reactjs.org"
-          target = "_blank"
-          rel = "noopener noreferrer"
-        >
-        </a>
-        <Form.Label>kuchaw</Form.Label>
-        <Form.Control type = "text"></Form.Control>
-        <Button variant="primary">press the button if you said hi</Button>{''}
+        <a></a>
+        <Primer_forms carroNombre = {carroNombre}/>
       </header>
     </div>
   );
